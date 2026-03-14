4c2cbb1d08d54550e6ed6f4c5e3893505b6e0feb
yaroslavlarionov <dlyasharagi39@gmail.com>
2026-03-14 18:35:41 +0300

    a

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.types import Message
 from requests import get
 from random import choice
 from aiogram import F
+import os
 # dp.run_polling()
 
 
@@ -13,14 +14,40 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    @dp.message(F.text.contains("start") )
-    async def start_handler(message: Message):
-        await message.answer(f"Привет {F.from_user.username}")
-    await dp.start_polling(bot)
+    # async def get_photo(message: Message, bot: Bot):
+    #     print(f"[LOG] Пользователь {message.from_user.id} вызвал get_photo")
+    #     photo = message.photo[-1]
+    #     file = await bot.get_file(photo.file_id)
+    #     print(f"[LOG] получение файла {file.file_unique_id}")
+    #     PATH = os.path.join("file", f"{file.file_unique_id}.jpg")
+    #     await bot.download_file(file.file_path, destination = "files/file1.jpg")
+    #     print(f"[LOG] сохранение файла {PATH}")
+    #     await message.answer("Крутое фото!")
+    #
+    # @dp.message(F.video)
+    # async def get_video(message: Message, bot: Bot):
+    #     print(f"[LOG] Пользователь {message.from_user.id} вызвал get_photo")
+    #     video = message.video[-1]
+    #     file = await bot.get_file(video.file_id)
+    #     print(f"[LOG] получение файла {file.file_unique_id}")
+    #     PATH = os.path.join("file", f"{file.file_unique_id}.mp4")
+    #     await bot.download_file(file.file_path, destination=PATH)
+    #     print(f"[LOG] сохранение файла {PATH}")
+    #     await message.answer("Крутое видео!")
 
-    @dp.message(F.from_user.username == "yarchhee")
-    async def get_me_stat(message: Message):
-        await message.answer("Вот тебе стата")
+    @dp.message(F.photo | F.video)
+    async def get_photo(message: Message):
+        os.makedirs("downloads", exist_ok=True)
+
+        if message.photo:
+            file = await bot.get_file(message.photo[-1].file_id)
+            PATH = os.path.join("downloads", f"{file.file_unique_id}.jpg")
+        else:
+            file = await bot.get_file(message.video.file_id)
+            PATH = os.path.join("downloads", f"{file.file_unique_id}.mp4")
+        await bot.download_file(file.file_path, destination = PATH)
+        await message.answer("Крутое фото или видео")
+
 
 print(f"[LOG] Бот запущен")
 run(main()) # запускает цикл событий
---
+++ test_os.py
@@ -0,0 +1,10 @@
+import os
+PATH = "files"
+# os.makedirs(PATH, exist_ok=True) # создание директории
+
+# print(os.path.exists("main.py")) # проверка файла / папки
+# if not os.path.exists(PATH):
+#     os.makedirs(PATH)
+# file_name = "test.txt"
+# print(os.path.join(PATH, file_name)) # создание пути в диретории
+
