c99205ccf73ebb3228ae7af8f017fc0e270fd625
yaroslavlarionov <dlyasharagi39@gmail.com>
2026-03-23 18:02:36 +0300

    a

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile,ReplyKeyboardMarkup, KeyboardButton
 from asyncio import run
 from config import BOT_TOKEN
 import os
@@ -10,98 +10,25 @@ from random import randint
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-    # @dp.message(F.photo | F.video | F.voice)
-    # async def get_photo_video_voice(message: Message, bot: Bot):
-    #     print(f"[LOG] Пользователь {message.from_user.id} вызвал функцию get_photo_video_voice")
-    #     os.makedirs("downloads", exist_ok=True)
-    #     if message.photo:
-    #         file = await bot.get_file(message.photo[-1].file_id)
-    #         print(f'[LOG] Файл {file.file_unique_id} получен')
-    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.jpg")
-    #     elif message.voice:
-    #         file = await bot.get_file(message.voice.file_id)
-    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.ogg")
-    #     else:
-    #         file = await bot.get_file(message.video.file_id)
-    #         print(f'[LOG] Файл {file.file_unique_id} получен')
-    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.mp4")
-    #     print(f"[LOG] начало скачивания {file}, по пути {PATH}")
-    #     await bot.download_file(file.file_path, destination=PATH)
-    #     print(f'[LOG] Файл {PATH} сохранен в соответствующую директорию')
-    #
-    #     await message.answer("крутое фото или видео или гс")
-    # @dp.message(F.sticker)
-    # async def get_sticker(message: Message):
-    #     print(f"[LOG] пользователь {message.from_user.id} вызвал функцию get_sticker")
-    #     with open("stickers.txt", "a+") as f:
-    #         f.write(message.sticker.file_id + "\n")
-    #         print(f"[LOG] записан стикер {message.sticker.file_id}")
-    # @dp.message(F.text == "отправь фото")
-    # async def send_photo(message: Message):
-    #     print(f"[LOG] получен запрос от {message.from_user.id} в send_photo")
-    #     PATH = os.path.join("downloads","123123.jpg")
-    #     print(f"[LOG] начало бинаризации")
-    #     photo=FSInputFile(PATH)
-    #     print(f"[LOG] конец бинаризации")
-    #     await message.answer_photo(photo=photo,caption="это география")
-    # #     await message.answer_photo("https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/11582/production/_103424017_mary-mcgowan_caught-in-the-act_00001294.jpg.webp",caption="это белка")
-    @dp.message(Command(commands=["start"]))
-    async def start_handler(message: Message):
-        print(f"[LOG] Пользователь нажал /start")
-        await message.answer("Привет")
-    @dp.message(Command(commands=["show"]))
-    async def show_command(message: Message):
-        msg = await message.answer("Загрузка")
-        with open("data.txt", mode="w") as file:
-            #randint
-            file.write("curs:temperature\n")
-            for _ in range(10):
-                file.write(f"{randint(0,1000)}:{randint(-30,30)}\n")
-        with open("data.txt",mode = "r") as file:
-            list_data = file.readlines()
-            if len(list_data) <= 1:
-                await message.answer("Нету данных в файле")
-            for i in list_data[1:]:
-                elements = i.split(":")
-                await msg.edit_text(f"Текущая температура на улице: {elements[1]}")
-                await asyncio.sleep(0.1)
-        await msg.delete()
+    knopka_1 = KeyboardButton(text = 'Команда 1')
+    keyboard =  ReplyKeyboardMarkup(keyboard = [[knopka_1]])
 
-    @dp.message(F.text.lower().contains("контакт"))
-    async def contact_handler(message: Message):
-        print(f"[LOG] Пользователь запросил контакт")
-        await message.answer ("Вот контакт")
-        await message.answer_contact(
-            phone_number="+79216112453",
-            first_name="Koshka"
+    @dp.message(Command(commands = 'start'))
+    async def command_start(message: Message):
+        await message.answer(
+            text = "вот",
+            reply_markup = keyboard
         )
 
-    @dp.message(F.text.lower().contains("адрес"))
-    async def location_handler(message: Message):
-        print(f"[LOG] Пользователь запросил адрес")
-        await message.answer("Вот тебе локация")
-        await message.answer_location(
-            latitude = 10,
-            longitude = 100
+    @dp.message(F.text == "Команда 1")
+    async def command(message: Message):
+        await message.answer(
+            text = "ты выбрал первую команду",
+            reply_markup = keyboard
         )
-    @dp.message()
-    async def anything(message: Message):
-        print(f"[LOG] Пользователь написал непонятно что")
-        await message.answer(message.text)
-
-        @dp.message(Command(commands=["silka"]))
-        async def silka_handler(message: Message):
-            msg = await message.answer("Загрузка...")
-            with open("task.txt", mode="r") as file:
-                s = file.readlines()
-                for i in s:
-                    num, link = i.split(" ")
-                    await asyncio.sleep(5)
-                    await msg.edit_text(f"Для ссылки {link} загружены данные")
-
     await dp.start_polling(bot)
 print(f'[LOG] Бот запущен')
 
 
-# if name == '__main__':
-#     run(main()) # запускает цикла событий(dispatcher)
+if name == '__main__':
+    run(main()) # запускает цикла событий(dispatcher)
