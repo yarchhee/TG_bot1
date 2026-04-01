ccaaa81efc787dc731063db8370bf2b8d66d7cdd
yaroslavlarionov <dlyasharagi39@gmail.com>
2026-04-01 19:20:19 +0300

    a

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, BotCommand, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, BotCommand, ReplyKeyboardRemove, \
+    inline_keyboard_button, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from asyncio import run
 from config import BOT_TOKEN
 from aiogram.filters import Command
@@ -17,71 +18,41 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    knopka_1 = KeyboardButton(text = 'Заказать еду')
-    knopka_2 = KeyboardButton(text = 'Пицца')
-    knopka_3 = KeyboardButton(text = "Суши")
-    knopka_4 = KeyboardButton(text = "Назад")
-
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=[[knopka_1]],  # Передаем туда кнопки, формируем клавиатуру
-        resize_keyboard=True,  # сжалась кнопка до высоты текста и ширины экрана телефона
-        input_field_placeholder="Клавиатура есть в плейсходлере ..."
-    )
-    keyboard_2 = ReplyKeyboardMarkup(
-        keyboard = [[knopka_2], [knopka_3], [knopka_4]],
-        resize_keyboard=True,
+    btn_1 = InlineKeyboardButton(
+        text = "Пицца",
+        callback_data="food_1"
     )
 
-    @dp.message(Command(commands=['start']))
-    async def start(message: Message):
-        await message.answer(
-            text = "Вот бот",
-            reply_markup=keyboard
-        )
+    btn_2 = InlineKeyboardButton(
+        text = "Суши",
+        callback_data = "food_2"
+    )
 
-    @dp.message(F.text == "Заказать еду")
-    async def zakazat(message: Message):
-        await message.answer(text = 'Выбирай',
-            reply_markup = keyboard_2
-        )
+    # btn_3 = InlineKeyboardButton(
+    #     text="Суп",
+    #     url=""
+    # )
 
-    @dp.message(F.text == "Пицца")
-    async def pizza(message: Message):
-        await message.answer_photo(photo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIlg5ZsLdACP_BM5qEPoLV5n_W06XozzL1zg&s')
-    @dp.message(F.text == "Суши")
-    async def sushi(message: Message):
-        await message.answer_photo(photo = 'https://sushiwok.ru/img/5717b143e944ea4ea04cff6c1112a584')
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[btn_1, btn_2]]
+    )
 
-    @dp.message(F.text == "Назад")
-    async def nazad(message: Message):
-        await start(message)
+    @dp.message(F.text)
+    async def text_handler(message: Message):
+        text = message.text
+        await message.answer("Вот клава", reply_markup=keyboard)
 
-    # @dp.message(F.text == 'Команда 1')
-    # async def com1_handler(message: Message):
-    #     print(message.contact.phone_number)
-    #     await message.answer(
-    #         text = "Вот команда 1",
-    #         reply_markup=ReplyKeyboardRemove()
-    #     )
-    #
-    # @dp.message(F.contact)
-    # async def get_contacts(message: Message):
-    #     data = message.contact.phone_number
-    #     print(data)
-    #
-    #
-    # @dp.message(F.location)
-    # async def loc_handler(message: Message):
-    #     loc_1 = message.location.latitude
-    #     loc_2 = message.location.longitude
-    #     print(loc_1, loc_2)
-    #
-    # @dp.message(F.text == "Команда 2")
-    # async def com2_handler(message: Message):
-    #     print(message.contact.location)
-    #     await message.answer('Ваши данные успешно украдены')
-
-
+    @dp.callback_query(F.data.startswit('food'))
+    async def callback_handler(callback: CallbackQuery):
+        # callback.answer
+        # callback.message.answer()
+        # await callback.message.edit_text("Ваш заказ оформлен")
+        data = callback.data
+        if data == "food_1":
+            await callback.message.answer("Вам отправили пиццу")
+        if data == "food_2":
+            await callback.message.answer("Вам отправили суши")
+        print(data)
 
     await dp.start_polling(bot)
 print(f'[LOG] Бот запущен')
