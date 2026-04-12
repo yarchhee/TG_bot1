ca84423e52b966d9d134c00373b6ad4a0e01781e
yaroslavlarionov <dlyasharagi39@gmail.com>
2026-04-12 14:52:27 +0300

    a

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    btn_1 = KeyboardButton(text = "📚 Наши курсы")
+    btn_2 = KeyboardButton(text = "ℹ️ О нас")
+    btn_3 = KeyboardButton(text = "💼 Примеры проектов")
+
+    menu_buttons = ReplyKeyboardMarkup(keyboard=[[btn_1], [btn_2], [btn_3]], resize_keyboard=True)
+
+    @dp.message(Command("start"))
+    async def start_handler(message: Message):
+        await message.answer("Приветствую вас в боте! Выберите то, что вам нужно.", reply_markup = menu_buttons)
+    @dp.message(F.text == "📚 Наши курсы"):
+        async def cours_handler(message : Message):
+            await message.answer("На данный момент доступны к записи вот эти курсы. Нажмите на понравившийся")
 
     await dp.start_polling(bot)
 
